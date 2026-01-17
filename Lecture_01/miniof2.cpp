2c625a31c031ed76718f700c463de65bd7137c6a
tejaspanchbhai0027 <tejastp2141@gmail.com>
2026-01-17 08:28:34 +0530

    minimum number between two number

---
+++ Lecture_01/miniof2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b ;
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter Second number";
+    cin>>b;
+    if(a<b)
+    {
+        cout<<"a is minimum";
+    }else{
+        cout<<"b is minimum";
+    }
+    return 0;
+}
