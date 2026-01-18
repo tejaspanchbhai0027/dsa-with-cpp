38bf33691a7759db6f59b15c5f6bf08503d973fd
tejaspanchbhai0027 <tejastp2141@gmail.com>
2026-01-18 15:52:17 +0530

    number is even or odd

---
+++ Lecture_01/evenorodd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number :";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"Even";
+    }else{
+        cout<<"odd";
+    }
+    return 0;
+}
