fadbbc751ae0b6d653563a7e53fcde3cdddb43f9
tejaspanchbhai0027 <tejastp2141@gmail.com>
2026-01-20 21:00:21 +0530

    postive negative number

---
+++ Lecture_01/posneg.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    char result;
+    cout<<"Enter the num : ";
+    cin>>num;
+    cout<<((num>=0)?"pos":"neg");
+    return 0;
+}
