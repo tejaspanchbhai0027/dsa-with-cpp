7f4110e7d6d43850135b5eba5ed30fd16710ac7f
tejaspanchbhai0027 <tejastp2141@gmail.com>
2026-01-30 09:05:16 +0530

    subtration of 3 number

---
+++ Lecture_01/sub_of_3num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,num3,sub;
+    cout<<"enter thr value of num one : ";
+    cin>>num1;
+    cout<<"enter thr value of num two : ";
+    cin>>num2;
+    cout<<"enter thr value of num there : ";
+    cin>>num3;
+    sub = num1-num2-num3;
+    cout<<"substration is : "<<sub;\
+    return 0;
+}
